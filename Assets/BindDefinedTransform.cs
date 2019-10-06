fad3e7e0dedbaca18826bed7747d868e560761dd
Isaac Cohen <isaaclandoncohen@gmail.com>
2019-10-05 23:21:12 -0600

    flokn

---
+++ Assets/BindDefinedTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindDefinedTransform : Binder
+{ 
+    public string transformName;
+    public Transform transformToBind;
+    public Matrix4x4 transformMatrix;
+    public override void Bind(){
+      toBind.BindMatrix( transformName, () => transformToBind.worldToLocalMatrix );
+    }
+
+
+    /*public override void WhileLiving( float v){
+//      print(transform.localToWorldMatrix[0]);
+      transformMatrix = transform.localToWorldMatrix;
+    }*/
+
+
+  }
